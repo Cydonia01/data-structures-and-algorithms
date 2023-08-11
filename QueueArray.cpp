2d6f48df02dd172c99f18fde78728b45d61e0c62
Mehmet Ali Özdemir <malizdmr011@gmail.com>
2023-08-11 22:47:26 +0300

    sampling

--- QueueArray.cpp
+++ QueueArray.cpp
@@ -31,7 +31,8 @@ public:
 	}
 	
 	void enqueue(int x) {
-		if (isFull()) cout<<"Queue is full.";
+		if (isFull())
+			cout<<"Queue is full.";
 		else {
 			rear = (rear + 1) % size;
 			A[rear] = x;
@@ -40,22 +41,24 @@ public:
 	
 	int dequeue() {
 		int x = -1;
-		if (isEmpty()) cout<<"Queue is empty.\n";
+		if (isEmpty())
+			cout<<"Queue is empty.\n";
 		else {
 			front = (front + 1) % size;
 			x = A[front];
-			
 		}
 		return x;
 	}
 	
 	int isEmpty() {
-		if (front == rear) return 1;
+		if (front == rear)
+			return 1;
 		return 0;
 	}
 	
 	int isFull() {
-		if ((rear + 1) % size == front) return 1;
+		if ((rear + 1) % size == front)
+			return 1;
 		return 0;
 	}
 	
@@ -69,6 +72,31 @@ public:
 };
 
 int main() {
-
+	// Sample: Since this is circular queue. The actual size of queue is given size minus 1.
+	
+	QueueArray q(5); // actual size is 4.
+	q.enqueue(1);
+	q.enqueue(2);
+	q.enqueue(3);
+	q.enqueue(4);
+	
+	q.Display();
+	
+	q.dequeue();
+	q.dequeue();
+	
+	q.Display();
+	
+	q.enqueue(5);
+	q.enqueue(6);
+	
+	q.Display();
+	
+	cout<<"front element is "<<q.first()<<endl;
+	cout<<"rear element is "<<q.last()<<endl;
+	
+	if (q.isFull())
+		cout<<"Queue is full.";
+	
 	return 0;
 }
